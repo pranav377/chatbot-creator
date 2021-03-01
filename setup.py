9a266d5b6f2100540aff64d72cd785b6069ad122
Pranava Mohan <71634384+pranav377@users.noreply.github.com>
2021-03-01 09:51:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="chatbot-creator",
-    version="0.0.5",
+    version="0.0.6",
     author="Pranava Mohan",
     license="MIT",
     description="Python package for creating chatbots.(including DiscordBot).",
